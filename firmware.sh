ded359496a01a3f52d7177adac211f3415207cd3
hijxdeladron <154901888+hijxdeladron@users.noreply.github.com>
2024-01-11 22:47:33 +0100

    Update firmware.sh

--- firmware.sh
+++ firmware.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Firmware
-cd /home/$USER/Descargas/
+cd /home/$USER/
 git clone https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
 cd linux-firmware
 sudo rm -rf .git
